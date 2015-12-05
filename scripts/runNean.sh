e2a0011c5578595b8b5fbaff9952e20e0492df8f
pratas <pratas@ua.pt>
2015-12-05 22:22:13 +0000

    runNeander

--- scripts/runNean.sh
+++ scripts/runNean.sh
@@ -2,20 +2,25 @@
 MYMAIL="pratas@ua.pt";
 #
 # INSTALL =====================================================================
-#
 . install.sh
 #
 # DOWNLOAD ====================================================================
 perl downloadViruses.pl
-echo "Downloaded virus base to sapiens ~/COMPLETE-NEANDERTHAL" | mail -s "Downloaded virus base..." $MYMAIL
+echo "Downloaded virus base to sapiens ~/COMPLETE-NEANDERTHAL" | \
+mail -s "Downloaded virus base..." $MYMAIL
 perl downloadBacteria.pl
-echo "Downloaded bacteria base to sapiens ~/COMPLETE-NEANDERTHAL" | mail -s "Downloaded bacteria base..." $MYMAIL
-. GetHumanNeand.sh
-echo "Downloaded neanderthal base to sapiens ~/COMPLETE-NEANDERTHAL" | mail -s "Downloaded neanderthal base..." $MYMAIL
-# =============================================================================
-
+echo "Downloaded bacteria base to sapiens ~/COMPLETE-NEANDERTHAL" | \
+mail -s "Downloaded bacteria base..." $MYMAIL
+. GetHumanNean.sh
+echo "Downloaded neanderthal base to sapiens ~/COMPLETE-NEANDERTHAL" | \
+mail -s "Downloaded neanderthal base..." $MYMAIL
+#
+# RUN =========================================================================
 for((xi=1 ; xi<=56 ; ++xi));
   do
-  (time ./FALCON -v -l 23 -p 1 -t 500 -n 10 -c 50 -x TOP$xi HN-C$xi viruses.fa) &> REPORT$xi;
+  (time ./FALCON -v -l 23 -p 1 -t 500 -n 10 -c 50 -x TOP$xi HN-C$xi \
+  viruses.fa) &> REPORT$xi;
   cat TOP$xi | mail -s "Neanderthal C$xi done! (COMPLETE-NEANDERTHAL)" $MYMAIL
   done
+#
+#==== =========================================================================
