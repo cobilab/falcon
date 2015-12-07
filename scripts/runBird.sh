3bb164009afbe0e84d69dddccc8f68bdd98b1bf3
pratas <pratas@ua.pt>
2015-12-07 16:27:48 +0000

    eagle

---
+++ scripts/runBird.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# 
+# AN EXAMPLE SHOWING THE DETECTION OF A VIRUS IN THE BALD EAGLE GENOME
+# FOR THE PURPOSE IT IS ONLY REQUIRED TO RUN THIS SCRIPT
+#
+# INSTALL FALCON ==============================================================
+git clone https://github.com/pratas/falcon.git
+cd falcon/src/
+cp Makefile.linux Makefile
+make
+cp FALCON ../../
+cd ../../
+cp falcon/scripts/downloadViruses.pl .
+rm -fr falcon/
+# GET BALD EAGLE ==============================================================
+WGETO=" --trust-server-names -q ";
+BEPAT=" ftp://climb.genomics.cn/pub/10.5524/101001_102000/101040"
+wget $WGETO $BEPAT/Haliaeetus_leucocephalus.fa.gz -O BE.fa.gz
+gunzip BE.fa.gz
+# GET VIRUSES =================================================================
+perl downloadViruses.pl
+# RUN FALCON ==================================================================
+./FALCON -v -l 4 -n 4 -t 10 -x top.csv BE.fa viruses.fa
+# ============================================================================= 
