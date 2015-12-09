58ea3cf2d237235459fd6f1e2e66a0dd45b31109
pratas <pratas@ua.pt>
2015-12-09 12:04:40 +0000

    echo

--- scripts/Example1.sh
+++ scripts/Example1.sh
@@ -15,8 +15,10 @@ rm -fr falcon/
 # GET BALD EAGLE ==============================================================
 WGETO=" --trust-server-names -q ";
 BEPAT=" ftp://climb.genomics.cn/pub/10.5524/101001_102000/101040"
+echo "Downloading and uncompressing Bald Eagle ...";
 wget $WGETO $BEPAT/Haliaeetus_leucocephalus.fa.gz -O BE.fa.gz
 gunzip BE.fa.gz
+echo "Done!";
 # GET VIRUSES =================================================================
 perl downloadViruses.pl
 # RUN FALCON ==================================================================
