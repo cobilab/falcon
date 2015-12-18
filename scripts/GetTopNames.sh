0dec22a46f45c06ec961ed4a97a88acb9e6b75ab
pratas <pratas@ua.pt>
2015-12-18 17:16:57 +0000

    getTopNames

--- scripts/GetTopNames.sh
+++ scripts/GetTopNames.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+# =============================================================================
+# 
+# GET THE MOST INFLUENT NAMES FROM TOP CACHE LIST GIVEN SCORE AND SIZE
 # EDIT <THIS> IN LINE 8 : if($3 > <THIS>), FOR CUSTOM THRESHOLD 
+#
+# =============================================================================
 SIZE=13;
 for((x=1 ; x<=$SIZE ; ++x));
   do
@@ -15,7 +20,6 @@ for((x=1 ; x<=$SIZE ; ++x));
   | uniq -d \
   > UNIQ$x;
   done
-
 rm -f ALL-UNIQ UNIQUE;
 for((x=1 ; x<=$SIZE ; ++x));
   do
