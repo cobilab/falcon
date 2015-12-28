fecdf16686505031fd5a255bee61e7b3b65220e0
pratas <pratas@ua.pt>
2015-12-28 15:59:48 +0000

    forEachFilteredLineDownloadBase

--- scripts/FilterNeanderthal.sh
+++ scripts/FilterNeanderthal.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-. GetTopNames.sh 56
-mv UNIQUE UNIQUE-NEANDERTHAL;
-# FIND NAMES
+#. GetTopNames.sh 56
+cp UNIQUE UNIQUE-NEANDERTHAL;
+filename="UNIQUE-NEANDERTHAL"
+while read -r line
+do
+    name=$line
+    if [[ $line == "" ]] 
+      then
+      continue;
+      fi;
+    echo "PROCESSING: $name"
+    perl downloadOrg.pl "$name"
+
+    echo "DONE";
+done < "$filename"
+
