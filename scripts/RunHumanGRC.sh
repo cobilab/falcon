6cbce7aeb4a0e71fc4087b41bd26f1c0e8ebff34
pratas <pratas@ua.pt>
2015-12-29 15:36:44 +0000

    RunHumanGrc

---
+++ scripts/RunHumanGRC.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+git clone https://github.com/pratas/falcon.git
+cd falcon/src/
+cmake .
+make
+cd ../../
+cp falcon/src/FALCON .
+cp falcon/scripts/GetHumanGRCParse.sh .
+cp falcon/scripts/downloadBacteria.pl .
+rm -fr falcon/
+perl downloadBacteria.pl
+. GetHumanGRCParse.sh
+rm -f GRC;
+for((x=1; x<=23 ; ++x)); do cat GRC$x >> GRC; rm -f GRC$x; done
+(time ./FALCON -l 38 -c 90 -n 1 -t 200000 -x TOP-GRC GRC Bacteria.fa ) &> REPORT-GRC
