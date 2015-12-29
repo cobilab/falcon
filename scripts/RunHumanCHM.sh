174960416e5c516c1abae4bd4a2cf374c777eb62
pratas <pratas@ua.pt>
2015-12-29 15:33:24 +0000

    automatic

--- scripts/RunHumanCHM.sh
+++ scripts/RunHumanCHM.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-. install.sh
+git clone https://github.com/pratas/falcon.git
+cd falcon/src/
+cmake .
+make
+cd ../../
+cp falcon/src/FALCON .
+cp falcon/scripts/GetHumanCHMParse.sh .
+cp falcon/scripts/downloadBacteria.pl .
+rm -fr falcon/
+perl downloadBacteria.pl
 . GetHumanCHMParse.sh
 rm -f CHM;
 for((x=1; x<=23 ; ++x)); do cat CHM$x >> CHM; rm -f CHM$x; done
