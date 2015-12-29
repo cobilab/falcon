5ce350fe0ac1ee685619d4329999ada5674aa9c1
pratas <pratas@ua.pt>
2015-12-29 15:50:08 +0000

    verbose

--- scripts/RunHumanCHM.sh
+++ scripts/RunHumanCHM.sh
@@ -12,4 +12,4 @@ perl downloadBacteria.pl
 . GetHumanCHMParse.sh
 rm -f CHM;
 for((x=1; x<=23 ; ++x)); do cat CHM$x >> CHM; rm -f CHM$x; done
-(time ./FALCON -l 38 -c 90 -n 1 -t 200000 -x TOP-CHM CHM Bacteria.fa ) &> REPORT-CHM
+(time ./FALCON -v -l 38 -c 90 -n 1 -t 200000 -x TOP-CHM CHM Bacteria.fa ) &> REPORT-CHM
