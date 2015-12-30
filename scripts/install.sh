d6218c9bd5df1c51e719264649f5c4e2f6a7c788
pratas <pratas@ua.pt>
2015-12-30 22:51:56 +0000

    installRef

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-rm -fr master.zip falcon-master/
-wget https://github.com/pratas/falcon/archive/master.zip
-unzip master.zip
-cd falcon-master/src
-cp Makefile.linux Makefile
+git clone https://github.com/pratas/falcon.git
+cd falcon/src/
+cmake .
 make
 cp FALCON ../../
 cd ../../
-rm -fr master.zip falcon-master/
