5fc8203629baec72b7c4097d106963a13ae00d83
pratas <pratas@ua.pt>
2016-04-13 17:23:51 +0100

    msg

--- src/falcon-eye.c
+++ src/falcon-eye.c
@@ -115,7 +115,7 @@ int32_t main(int argc, char *argv[]){
     }
   rewind(INPUT);
 
-  fprintf(stderr, "Filtered %"PRIu64" from %"PRIu64" entries.\n", 
+  fprintf(stderr, "Skipping %"PRIu64" from %"PRIu64" entries.\n", 
   filtered, filtered+nSeq);
 
   Paint = CreatePainter(maxSize, PEYE->width, PEYE->space, PEYE->proportion, 
