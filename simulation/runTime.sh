e8f76ae6d6ae820ed2756242cbeeb656cce5dae3
pratas <pratas@ua.pt>
2017-03-07 17:12:56 +0000

    renew

--- simulation/runTime.sh
+++ simulation/runTime.sh
@@ -7,16 +7,13 @@
 # PARAMETERS ==================================================================
 RENEW=1;
 INSTALL=1;
-SIMULATE=1;
-SHUFFLE=1;
-FALCON=1;
-GREEN=1;
-MUMMER=1;
-MUMMER20=1;
+RUN=1;
 PLOT=1;
 #==============================================================================
-FQLINE=200;
-FQNREADS=1000;
+FQLINE=100;
+FQNREADS_MIN=200;
+FQNREADS_MAX=4000;
+FQNREADS_STEP=200;
 PMIN=30;   #PERMUTATION MINIMUM
 PMAX=250;  #PERMUTATION MAXIMUM
 JUMP=1;    #JUMP BETWEEN LOOPS
@@ -83,98 +80,105 @@ cp MUMmer3.23/show-coords .
 cp MUMmer3.23/delta-filter .
 fi
 ###############################################################################
-# SIMULATE ====================================================================
-if [[ "$SIMULATE" -eq "1" ]]; then
-./XS -v -ls $FQLINE -n $FQNREADS -f $DISTRIBUTION -s 0 SAMPLE.fq
-# MUTATE ======================================================================
-./goose-fastq2fasta < SAMPLE.fq > SAMPLE.fa
-./goose-fasta2seq   < SAMPLE.fa > SAMPLE
-./goose-seq2fasta -n "Permutation0" < SAMPLE > SAMPLE0.fa
-for((x=$PMIN ; x<=$PMAX ; x+=$JUMP));
+# RUN  ========================================================================
+if [[ "$RUN" -eq "1" ]]; then
+rm -f TIME_FALCON TIME_GREEN TIME_MUMMER TIME_MUMMER20;
+for((y=$FQNREADS_MIN ; y<=$FQNREADS_MAX ; y+=$FQNREADS_STEP));
   do
-  echo "Permutation rate: $x";
-  ./goose-permuteseqbyblocks -bs $x -s 101 < SAMPLE > SAMPLE$x
-  ./goose-seq2fasta -n "Permutation$x" < SAMPLE$x > SAMPLE$x.fa 
-  cat SAMPLE$x.fa >> DB.mfa;
+  ./XS -v -ls $FQLINE -n $y -f $DISTRIBUTION -s 0 SAMPLE.fq
+  # MUTATE ====================================================================
+  ./goose-fastq2fasta < SAMPLE.fq > SAMPLE.fa
+  ./goose-fasta2seq   < SAMPLE.fa > SAMPLE
+  ./goose-seq2fasta -n "Permutation0" < SAMPLE > SAMPLE0.fa
+  for((x=$PMIN ; x<=$PMAX ; x+=$JUMP));
+    do
+    echo "Permutation rate: $x";
+    ./goose-permuteseqbyblocks -bs $x -s 101 < SAMPLE > SAMPLE$x
+    ./goose-seq2fasta -n "Permutation$x" < SAMPLE$x > SAMPLE$x.fa
+    cat SAMPLE$x.fa >> DB.mfa;
+    done
+  . ShufFASTQReads.sh SAMPLE.fq > SAMPLE-SHUF.fq
+  mv SAMPLE-SHUF.fq SAMPLE.fq
+  ./goose-fastq2fasta < SAMPLE.fq > SAMPLE.fa
+  # RUN FALCON ================================================================
+  START=$(date +%s.%N)
+  ./FALCON -v -F $FPARAM -n 4 -t $PMAX -x TOP-SUBS SAMPLE.fq DB.mfa
+  cat TOP-SUBS | awk '{ print $4"\t"$3;}' | sed 's/\Permutation//g' | sort -n \
+  | awk '{ print $1"\t"$2;}' > TOP-SUBS-FILT;
+  END=$(date +%s.%N)
+  DIFF=$(echo "$END - $START" | bc)
+  echo "$y $DIFF" >> TIME_FALCON
+  # RUN GREEN =================================================================
+  rm -f TOP-GREEN;
+  cat SAMPLE.fa | grep -v ">" | tr -d -c "ACGTN" > SAMPLE.seq;
+  START=$(date +%s.%N)
+  GSIZE=`./goose-info < SAMPLE.seq  | grep "Number of" | awk '{ print $4;}'`;
+  for((x=$PMIN ; x<=$PMAX ; x+=$JUMP));
+    do
+    printf "%u\t" "$x" >> TOP-GREEN;
+    cat SAMPLE$x.fa | grep -v ">" | tr -d -c "ACGTN" > SAMPLEX.seq;
+    ./GReEnC -v $GREENPARAM SAMPLE.seq SAMPLEX.seq > TMPX;
+    GVALUE=`cat TMPX | grep "number of bytes" | awk '{ print $5;}'`;
+    GRES=`echo "scale=6;100-((8*($GVALUE)/(2*$GSIZE))*100)" | bc -l`;
+    echo $GRES >> TOP-GREEN;
+    done
+  END=$(date +%s.%N)
+  DIFF=$(echo "$END - $START" | bc)
+  echo "$y $DIFF" >> TIME_GREEN
+  # RUN MUMMER ================================================================
+  rm -f TOP-MUMMER;
+  START=$(date +%s.%N)
+  for((x=$PMIN ; x<=$PMAX ; x+=$JUMP));
+    do
+    printf "%u\t" "$x" >> TOP-MUMMER;
+    ./nucmer -p mummer-tmp SAMPLE.fa SAMPLE$x.fa
+    ./delta-filter -1 mummer-tmp.delta > mummer-tmp.delta2
+    ./show-coords -clr mummer-tmp.delta2 > mummer-tmp.delta3
+    echo "Running Global similarity for MUMmer ...";
+    . GlobalMUMmer.sh mummer-tmp.delta3 >> TOP-MUMMER;
+    done
+  END=$(date +%s.%N)
+  DIFF=$(echo "$END - $START" | bc)
+  echo "$y $DIFF" >> TIME_MUMMER
+  # RUN MUMMER 20 =============================================================
+  rm -f TOP-MUMMER20;
+  START=$(date +%s.%N)
+  for((x=$PMIN ; x<=$PMAX ; x+=$JUMP));
+    do
+    printf "%u\t" "$x" >> TOP-MUMMER20;
+    ./nucmer -c 20 -p mummer-tmp SAMPLE.fa SAMPLE$x.fa
+    ./delta-filter -1 mummer-tmp.delta > mummer-tmp.delta2
+    ./show-coords -clr mummer-tmp.delta2 > mummer-tmp.delta3
+    echo "Running Global similarity for MUMmer -c 20 ...";
+    . GlobalMUMmer.sh mummer-tmp.delta3 >> TOP-MUMMER20;
+    done
+  END=$(date +%s.%N)
+  DIFF=$(echo "$END - $START" | bc)
+  echo "$y $DIFF" >> TIME_MUMMER20
   done
-fi
-###############################################################################
-# SHUFFLE READS ===============================================================
-if [[ "$SHUFFLE" -eq "1" ]]; then
-. ShufFASTQReads.sh SAMPLE.fq > SAMPLE-SHUF.fq
-mv SAMPLE-SHUF.fq SAMPLE.fq
-./goose-fastq2fasta < SAMPLE.fq > SAMPLE.fa
-fi
-###############################################################################
-# RUN FALCON ==================================================================
-if [[ "$FALCON" -eq "1" ]]; then
-START=$(date +%s.%N)
-./FALCON -v -F $FPARAM -n 4 -t $PMAX -x TOP-SUBS SAMPLE.fq DB.mfa
-cat TOP-SUBS | awk '{ print $4"\t"$3;}' | sed 's/\Permutation//g' | sort -n \
-| awk '{ print $1"\t"$2;}' > TOP-SUBS-FILT;
-END=$(date +%s.%N)
-DIFF=$(echo "$END - $START" | bc)
-echo "total_time:" $DIFF > TIME_FALCON_$FQNREADS
-fi
-###############################################################################
-# RUN MUMMER ==================================================================
-if [[ "$MUMMER" -eq "1" ]]; then
-rm -f TOP-MUMMER;
-START=$(date +%s.%N)
-for((x=$PMIN ; x<=$PMAX ; x+=$JUMP));
-  do
-  printf "%u\t" "$x" >> TOP-MUMMER;
-  ./nucmer -p mummer-tmp SAMPLE.fa SAMPLE$x.fa
-  ./delta-filter -1 mummer-tmp.delta > mummer-tmp.delta2
-  ./show-coords -clr mummer-tmp.delta2 > mummer-tmp.delta3
-  echo "Running Global similarity for MUMmer ...";
-  . GlobalMUMmer.sh mummer-tmp.delta3 >> TOP-MUMMER;
-  done
-END=$(date +%s.%N)
-DIFF=$(echo "$END - $START" | bc)
-echo "total_time:" $DIFF > TIME_MUMMER_$FQNREADS
-fi
-###############################################################################
-# RUN MUMMER 20 ===============================================================
-if [[ "$MUMMER20" -eq "1" ]]; then
-rm -f TOP-MUMMER20;
-START=$(date +%s.%N)
-for((x=$PMIN ; x<=$PMAX ; x+=$JUMP));
-  do
-  printf "%u\t" "$x" >> TOP-MUMMER20;
-  ./nucmer -c 20 -p mummer-tmp SAMPLE.fa SAMPLE$x.fa
-  ./delta-filter -1 mummer-tmp.delta > mummer-tmp.delta2
-  ./show-coords -clr mummer-tmp.delta2 > mummer-tmp.delta3
-  echo "Running Global similarity for MUMmer -c 20 ...";
-  . GlobalMUMmer.sh mummer-tmp.delta3 >> TOP-MUMMER20;
-  done
-END=$(date +%s.%N)
-DIFF=$(echo "$END - $START" | bc)
-echo "total_time:" $DIFF > TIME_MUMMER20_$FQNREADS
 fi
 ###############################################################################
 # PLOT ========================================================================
 if [[ "$PLOT" -eq "1" ]]; then
 gnuplot << EOF
 set terminal pdfcairo enhanced color font 'Verdana,14'
-set output "per.pdf"
+set output "time.pdf"
 set auto
 set size ratio 0.75
 set key outside right center
-set yrange [0:100] 
-h(x)=(-x*log(x)-(1-x)*log(1-x))/log(2)
 unset grid
-set ylabel "Relative similarity (%)"
-set xlabel "Permutation block size"
+set ylabel "Time (s)"
+set xlabel "Number of reads"
 set style line 1 lt 2 lc rgb "#CA000D" lw 3
 set style line 2 lt 2 lc rgb "#018D0F" lw 3
 set style line 3 lt 2 lc rgb "#000DC0" lw 3
 set style line 4 lt 2 lc rgb "#FF811F" lw 3
 set style line 5 lt 2 lc rgb "black" lw 3
-plot [$PMAX:$PMIN] "TOP-MUMMER" u 1:2 w lines ls 3 title "MUMmer", \
- "TOP-MUMMER20" u 1:2 w lines ls 4 title "MUMmer -c 20", \
- "TOP-GREEN" u 1:2 w lines ls 2 title "GREEN", \
- "TOP-SUBS-FILT" u 2 w lines ls 1 title "FALCON"
+plot [$FQNREADS_MIN:$FQNREADS_MAX] "TIME_MUMMER" u 1:2 w lines ls 3 title "MUMmer", \
+ "TIME_MUMMER20" u 1:2 w lines ls 4 title "MUMmer -c 20", \
+ "TIME_GREEN" u 1:2 w lines ls 2 title "GREEN", \
+ "TIME_FALCON" u 1:2 w lines ls 1 title "FALCON"
 EOF
 fi
 #==============================================================================
+
