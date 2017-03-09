30ebe3fe1a4eefc0d0bb40140de352a440e9b443
pratas <pratas@ua.pt>
2017-03-09 15:12:17 +0000

    run With double of memory

--- src/models.h
+++ src/models.h
@@ -8,7 +8,8 @@
 #define ARRAY_MODE            0
 #define HASH_TABLE_MODE       1
 #define HASH_TABLE_BEGIN_CTX  15
-#define HASH_SIZE             33554471
+//#define HASH_SIZE             33554471
+#define HASH_SIZE             67108879
 #define MAX_COLLISIONS        10
 
 #if defined(PREC32B)
