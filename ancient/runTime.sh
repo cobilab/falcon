75456de6e06a29e607c96fe2690bca5f4ab481a1
pratas <pratas@ua.pt>
2017-04-12 16:39:53 +0100

    time

---
+++ ancient/runTime.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#
+READS_FILE="/home/pratas/DATABASE/NEANDERTHAL/FASTQ/NEAN.fq";
+GET_FALCON=1;
+GET_BOWTIE=1;
+GET_ECOLI=1
+RUN_FALCON=1;
+RUN_BOWTIE=1;
+#
+#==============================================================================
+# GET BOWTIE
+#
+if [[ "$GET_BOWTIE" -eq "1" ]]; then
+  #sudo apt-get install libtbb-dev
+  rm -fr bowtie/
+  git clone https://github.com/BenLangmead/bowtie.git
+  cd bowtie/
+  make
+  cp bowtie ../bowtiex
+  cp bowtie-build ../bowtiex-build
+  cd ..
+fi
+#==============================================================================
+# GET FALCON
+#
+if [[ "$GET_FALCON" -eq "1" ]]; then
+  rm -fr falcon FALCON FALCON-* *.pl
+  git clone https://github.com/pratas/falcon.git
+  cd falcon/src/
+  cmake .
+  make
+  cp FALCON ../../
+  cp FALCON-FILTER ../../
+  cp FALCON-EYE ../../
+  cd ../../
+  cp falcon/scripts/*.pl .
+fi
+#==============================================================================
+# DOWNLOAD ECOLI
+#
+if [[ "$GET_ECOLI" -eq "1" ]]; then
+  rm -f GCF_000005845.2_ASM584v2_genomic.fna.* ECOLI.fna;
+  wget ftp://ftp.ncbi.nlm.nih.gov/genomes/refseq/bacteria/Escherichia_coli/reference/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
+  gunzip GCF_000005845.2_ASM584v2_genomic.fna.gz
+  mv GCF_000005845.2_ASM584v2_genomic.fna ECOLI.fa
+fi
+#==============================================================================
+# RUN BOWTIE
+#
+if [[ "$RUN_BOWTIE" -eq "1" ]]; then
+  (time ./bowtiex-build ECOLI.fa index-ECOLI ) > REPORT_BOWTIE_1;
+  (time ./bowtiex -a -v 3 --sam index-ECOLI $READS_FILE > OUT_ALIGNED.sam ) &> REPORT_BOWTIE_2;
+fi
+#
+#==============================================================================
+# RUN FALCON
+#
+if [[ "$RUN_FALCON" -eq "1" ]]; then
+  (time ./FALCON -v -n 8 -t 1000 -F -m 20:100:1:5/10 -c 200 $READS_FILE ECOLI.fa ) &> REPORT_FALCON;
+fi
+#==============================================================================
+
